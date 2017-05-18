b0a60b08a57aceaa30b749505a25fba10143b227
efsalvarenga <efsa@bath.edu>
2017-05-18 17:03:45 +0100

    Revert "17051816 before finished charts - integration win and mac"
    This reverts commit 70a2e23002172d728fcfb6b9b734153a1e8c5f43.

--- KSSM1000_combfun_v01.R
+++ KSSM1000_combfun_v01.R
@@ -26,7 +26,6 @@ library(rgenoud)
 ptm <- proc.time() # Start the clock!
 cl  <- makeCluster(detectCores())
 registerDoParallel(cl)
-setwd("~/Google Drive/Bath/research/demand_forecasting/kssm_rdev")
 
 # From KSSM0100
 wm01_00       <- readRDS("0100_import-complete.rds")
@@ -40,8 +39,8 @@ data_size     <- importpar[5]
 #===========================================
 # Integrated Parameters
 #===========================================
-cus_list      <- seq(1,20)
-frontierstp   <- 8                       # Number of demand bins (Stepwise frontier for portfolio optimisation)
+cus_list      <- seq(1,10)
+frontierstp   <- 3                       # Number of demand bins (Stepwise frontier for portfolio optimisation)
 win_size      <- c(4,24)                 # Small and large win_size (select only 2)
 ahead_t       <- seq(1, (24/sum_of_h))   # Up to s02
 hrz_lim       <- seq(0,2)*137
@@ -271,7 +270,6 @@ wm01_02    <- wm01_02l[[1]] / rowSums(wm01_02l[[2]])
 wl06rnd    <- fx_int_cvfcst(wm01_02,h,in_sample_fr,s01,s02,sum_of_h,win_size,seas_bloc_ws,crossvalsize,T)
 wv45rnd    <- as.numeric(rowMeans(wl06rnd[[1]]) * rowSums(wm01_02l[[2]]))
 sd01rnd    <- as.numeric(fx_sd_mymat(wl06rnd[[3]]))
-cr01rnd    <- rowMeans(wl06rnd[[2]])
 
 #===========================================
 # Optimised groups forecast
@@ -288,53 +286,62 @@ optgrp_sdev  <- foreach (i = 1:frontierstp,
                                             print.level=1)
                          optgrp$par
                        }
+
 wm01_03l    <- list(optgrp_sdev %*% wm01_01, optgrp_sdev)
 wm01_03     <- wm01_03l[[1]] / rowSums(wm01_03l[[2]])
 wl06optsdev <- fx_int_cvfcst(wm01_03,h,in_sample_fr,s01,s02,sum_of_h,win_size,seas_bloc_ws,crossvalsize,T)
 wv45optsdev <- as.numeric(rowMeans(wl06optsdev[[1]]) * rowSums(wm01_03l[[2]]))
 sd01optsdev <- as.numeric(fx_sd_mymat(wl06optsdev[[3]]))
-cr01optsdev <- rowMeans(wl06optsdev[[2]])
 
-optgrp_crps  <- foreach (i = 1:frontierstp,
-                         .packages=c("forecast","rgenoud","foreach"),
-                         .combine=c("rbind")) %dopar% {
-                           opt_min_cusd  = wv46[i]
-                           opt_max_cusd  = wv46[i+1]
-                           optgrp   <- genoud(fx_optgrp_crps, nvars=nrow(wm01_01), max.generations=300, wait.generations=20,
-                                              Domains = cbind(c(rep(0,nrow(wm01_01))),c(rep(1,nrow(wm01_01)))),
-                                              data.type.int=TRUE,  int.seed=1,
-                                              print.level=1)
-                           optgrp$par
-                         }
-wm01_04l    <- list(optgrp_crps %*% wm01_01, optgrp_crps)
-wm01_04     <- wm01_04l[[1]] / rowSums(wm01_04l[[2]])
-wl06optcrps <- fx_int_cvfcst(wm01_04,h,in_sample_fr,s01,s02,sum_of_h,win_size,seas_bloc_ws,crossvalsize,T)
-wv45optcrps <- as.numeric(rowMeans(wl06optcrps[[1]]) * rowSums(wm01_04l[[2]]))
-sd01optcrps <- as.numeric(fx_sd_mymat(wl06optcrps[[3]]))
-cr01optcrps <- rowMeans(wl06optcrps[[2]])
+
+
+# opt_min_cusd <- min(wv46)
+# opt_max_cusd <- max(wv46)
+# wmoptsdev    <- foreach (i = 1:frontierstp, .combine=c("cbind")) %dopar% {
+#   c(fx_optgrp_sdev(optgrp_sdev[i,]),optgrp_sdev[i,] %*% wv45)
+# }
+
+# optgrp_crps  <- foreach (i = 1:frontierstp,
+#                          .packages=c("forecast","rgenoud"),
+#                          .combine=c("rbind")) %do% {
+#                            opt_min_cusd  = wv46[i]
+#                            opt_max_cusd  = wv46[i+1]
+#                            optgrp   <- genoud(fx_optgrp_crps, nvars=nrow(wm01_01), max.generations=300, wait.generations=20,
+#                                               Domains = cbind(c(rep(0,nrow(wm01_01))),c(rep(1,nrow(wm01_01)))),
+#                                               data.type.int=TRUE,  int.seed=1,
+#                                               print.level=1)
+#                            optgrp$par
+#                          }
+# opt_min_cusd <- min(wv46)
+# opt_max_cusd <- max(wv46)
+# wmoptcrps    <- foreach (i = 1:frontierstp, .combine=c("cbind")) %do% {
+#   c(fx_optgrp_crps(optgrp_crps[i,]),optgrp_crps[i,] %*% wv45)
+# }
+
+# 
+# teste <- foreach (i = 1:45,.combine=c("rbind")) %do%{
+#   c(fx_optgrp_crps(wm01_02l[[2]][i,]),fx_optgrp_sdev(wm01_02l[[2]][i,]))
+# }
+
+
+
 
 # fx_plt_mymat(wl06[[2]])
 # fx_plt_mymat(wl06rnd[[2]])
 
-rangecrps = range(min(as.numeric(c(cr01rnd,cr01optsdev,cr01optsdev))),max(as.numeric(c(cr01optsdev,cr01optsdev))))
-plot(rangecrps,range(0:ceiling(sum(wv45))), bty="n", type="n", xlab=paste("crps of out-sample"),
-     ylab="Mean Demand",main=paste("optimum vs random groups"))
-grid (NA,NULL, lty = 'dotted')
-points(cr01rnd,wv45rnd,col="red",pch=19)
-points(cr01optsdev,wv45optsdev,col="green",pch=19)
-points(cr01optcrps,wv45optcrps,col="blue",pch=19)
-legend('topright', inset=c(0,0), legend = c("random","opt_sdev","opt_crps"),
-       lty=1, col=c("red","green","blue"), bty='n', cex=.75, title="Groups")
+# plot(rowMeans(wl06[[2]]),wv45)
+plot(rowMeans(wl06rnd[[2]]),wv45rnd)
 
-rangesdev = range(min(as.numeric(c(sd01rnd,sd01optsdev,sd01optcrps))),max(as.numeric(c(sd01optsdev,sd01optcrps))))
-plot(rangesdev,range(0:ceiling(sum(wv45))), bty="n", type="n", xlab=paste("stdev of deseasonalised out-sample"),
+
+
+rangesdev = range(min(as.numeric(c(sd01rnd,sd01optsdev))),max(as.numeric(c(sd01rnd,sd01optsdev))))
+plot(rangesdev,range(0:ceiling(sum(wv45))), bty="n", type="n", xlab=paste("stdev of noise after decompose"),
      ylab="Mean Demand",main=paste("optimum vs random groups"))
 grid (NA,NULL, lty = 'dotted')
 points(sd01rnd,wv45rnd,col="red",pch=19)
-points(sd01optsdev,wv45optsdev,col="green",pch=19)
-points(sd01optcrps,wv45optcrps,col="blue",pch=19)
-legend('topright', inset=c(0,0), legend = c("random","opt_sdev","opt_crps"),
-       lty=1, col=c("red","green","blue"), bty='n', cex=.75, title="Groups")
+points(sd01optsdev,wmoptsdev[2,],col="green",pch=19)
+legend('topright', inset=c(-0.10,0), legend = c("random","opt_sdev"),
+       lty=1, col=c("red","green"), bty='n', cex=.75, title="Groups")
 
 print(proc.time() - ptm)        # Stop the clock
 
