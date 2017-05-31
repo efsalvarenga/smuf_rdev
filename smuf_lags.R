2342c1045458860defb6dc0f938b9ecc7a69c53a
efsalvarenga <efsa@bath.edu>
2017-05-31 18:23:39 +0100

    lags code ready

--- smuf_lags.R
+++ smuf_lags.R
@@ -34,7 +34,7 @@ data_size     <- importpar[5]
 #===========================================
 cus_list      <- seq(1,10)
 ahead_t       <- seq(1, (24/sum_of_h))   # Up to s02
-hrz_lim       <- seq(0,3)*2537
+hrz_lim       <- seq(0,1)*2537
 in_sample_fr  <- 1/6                     # Fraction for diving in- and out-sample
 seas_bloc_ws  <- 6                       # Number of weeks used for calculating seasonality pattern (6 seems best)
 maxlag        <- 5                       # Max lags analysed for ARIMA fit (ARMA-GARCH model)
@@ -63,12 +63,13 @@ fx_lags_armagarch <- function (wm04,maxlag,out_evhor){
     final.ord <- final.bic[sort.list(final.bic[,3]), ]
     c(final.ord[1,1],final.ord[1,2])
   }
-  return(c(max(lags_armagarch[,1]),max(lags_armagarch[,2])))
+  return(c(max(lags_armagarch[,1]),max(lags_armagarch[,2]),nrow(lags_armagarch)))
 }
 
 #===========================================
 # BIG [h] LOOP Start
 #===========================================
+wm01_01    <- wm01_00[min(cus_list):length(cus_list),]
 hlags      <- foreach (h = hrz_lim, .packages=c("rugarch","doParallel"), .combine=c("rbind")) %dopar% {
   out_evhor  <- fx_evhor(wm01_01,h,in_sample_fr,ahead_t,s02,seas_bloc_ws,0)
   wm01       <- wm01_01[,out_evhor[2]:out_evhor[3]]                       # work matrix
@@ -78,8 +79,10 @@ hlags      <- foreach (h = hrz_lim, .packages=c("rugarch","doParallel"), .combin
   fx_lags_armagarch(wm04,maxlag,out_evhor)
 }
 arma.lags <- c(max(hlags[,1]),max(hlags[,2]))
+cus_check <- hlags[,3]
 
 #===========================================
 # Outputs
 #===========================================
 saveRDS(arma.lags,  file="smuf_lags-arma.rds")
+print(proc.time() - ptm)
