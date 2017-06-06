c27cf949865404abec20d37414391eed09cc7b58
efsa@marte <efsa@bath.edu>
2017-06-06 11:17:09 +0100

    running _compare

--- smuf_compare.R
+++ smuf_compare.R
@@ -34,11 +34,11 @@ data_size     <- importpar[5]
 #===========================================
 # Integrated Parameters
 #===========================================
-cus_list      <- seq(1,20)
+cus_list      <- seq(1,100)
 # frontierstp   <- 5                       # Number of demand bins (Stepwise frontier for portfolio optimisation)
 win_size      <- c(4,24)                 # Small and large win_size (select only 2)
 ahead_t       <- seq(1, (24/sum_of_h))   # Up to s02
-hrz_lim       <- seq(0,1)*2069
+hrz_lim       <- seq(0,2)*2069
 in_sample_fr  <- 1/6                     # Fraction for diving in- and out-sample
 crossvalsize  <- 1                       # Number of weeks in the end of in_sample used for crossvalidation
 crossvalstps  <- 2                       # Steps used for multiple crossvalidation (Only KDE)
