27d4244363ceee77de4a1d234d372876d453324a
efsa@marte <efsa@bath.edu>
2017-06-22 21:37:40 +0100

    _aux for creating plots for paper

---
+++ smuf_aux.R
@@ -0,0 +1,34 @@
+#===========================================
+# Smart Metering Uncertainty Forecasting
+#
+# Author  Estevao "Steve" Alvarenga
+#         efsa@bath.edu
+# Created in 10/Feb/17
+#-------------------------------------------
+# smuf_aux create plots for paper
+#===========================================
+library(ggplot2)
+
+setwd("~/GitRepos/smuf_rdev")
+source("smuf_main-fxs.R")
+
+wm01_00       <- readRDS("smuf_import-complete.rds")
+importpar     <- readRDS("smuf_import-parameter.rds")
+s01           <- importpar[1]
+s02           <- importpar[2]
+s03           <- importpar[3]
+sum_of_h      <- importpar[4]
+data_size     <- importpar[5]
+
+cus_no  <- 1000
+ds_ini  <- 1001
+ds_len  <- s02*4
+
+plot1   <- as.data.frame(t(wm01_00[1:cus_no,ds_ini:(ds_ini+ds_len-1)]))
+plot1   <- cbind(seq(1,ds_len),plot1)
+colnames(plot1) <- c("Time",paste("Cus",1:cus_no,sep=""))
+ggplot(plot1, aes(Time,Cus1))+geom_line(color="firebrick") + 
+  labs(title="Example of demand pattern for one customer") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(x = "Hours (4 weeks)", y = "Energy demand (KWh)")
+
