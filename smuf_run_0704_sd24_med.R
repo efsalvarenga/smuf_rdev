c4c5b48b21e1ef4201561c9c695377e6e905d173
efsa@marte <efsa@bath.edu>
2017-07-06 17:47:42 +0100

    small fix to sd med

--- smuf_run_0704_sd24_med.R
+++ smuf_run_0704_sd24_med.R
@@ -33,9 +33,9 @@ frontierexp   <- 1.2                     # Exponentiality of frontier steps
 max.gen       <- 300                     # For genetic opt
 waitgen       <- 50                      # For genetic opt
 win_size      <- c(4,24)                 # Small and large win_size (select only 2)
-win_selec     <- win_size[1]
+win_selec     <- win_size[2]
 cross_overh   <- 4                       # Cross-over forced for fx_fcst_kds_quickvector
-ahead_t       <- seq(1, (2/sum_of_h))    # Up to s02
+ahead_t       <- seq(1, (24/sum_of_h))    # Up to s02
 hrz_lim       <- 0 #seq(5,6)*113            # Rolling forecasts steps {seq(0:167)*113} is comprehensive
 in_sample_fr  <- 1/6                     # Fraction for diving in- and out-sample
 crossvalsize  <- 1                       # Number of weeks in the end of in_sample used for crossvalidation
